cdc484e3ec87232abf870ea2bfe596bca7db08d5
onlyo <onlyo@RACHETA>
2024-03-24 19:10:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Configuration;
+
+namespace Aplicatie_TargDeMasini
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
