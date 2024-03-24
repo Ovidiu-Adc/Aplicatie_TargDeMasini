fe9def6b6b2c12143308ae4d6b11d05afd08b136
Ovidiu-Adc <adonicioaiegabriel@yahoo.com>
2024-03-24 19:48:58 +0200

    Laboratorul 2 - Clase necesare

---
+++ ClaseNecesare/Clase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseNecesare
+{
+    public class TranzactieAuto
+    {
+        public string NumeVanzator { get; set; }
+        public string NumeCumparator { get; set; }
+        public string TipMasina { get; set; }
+        public int AnFabricatie { get; set; }
+        public string Culoare { get; set; }
+        public string Optiuni { get; set; }
+        public DateTime DataTranzactie { get; set; }
+        public decimal Pret { get; set; }
+    }
+
+    public class RaportAuto
+    {
+        public void CeaMaiCautataMasina(string firmaSauModel, DateTime dataDeLa, DateTime dataPanaLa)
+        {
+            // cod ..
+        }
+
+        public void GraficPretPentruModel(string model, DateTime dataDeLa, DateTime dataPanaLa)
+        {
+            // cod ..
+        }
+
+        public void TranzactiiPeZi(DateTime zi)
+        {
+            // cod ..
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using ClaseNecesare;
 
 namespace Aplicatie_TargDeMasini
 {
